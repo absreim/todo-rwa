a42697d656999a3eeb779e48dd1757892cc9a708
Brook Li <emcdynamicist@gmail.com>
2024-06-24 02:22:18 -0400

    Working seed script example that does not require deleting database

---
+++ SeedDatabase/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TodoRwa.Models;
+
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+var postgresConnStr = config["Postgres:ConnectionString"];
+
+var contextOptions = new DbContextOptionsBuilder<TodoContext>()
+    .UseNpgsql(postgresConnStr).Options;
+var dbContext = new TodoContext(contextOptions);
+dbContext.Database.Migrate();
+using var transaction = dbContext.Database.BeginTransaction();
+
+try
+{
+    var testRow1 = new TodoItem
+    {
+        Name = "foo",
+        IsComplete = false
+    };
+    var testRow2 = new TodoItem
+    {
+        Name = "bar",
+        IsComplete = true
+    };
+    var testRow3 = new TodoItem
+    {
+        Name = null,
+        IsComplete = true
+    };
+
+    dbContext.Database.ExecuteSql($"DELETE FROM \"TodoItems\"");
+    dbContext.TodoItems.Add(testRow1);
+    dbContext.TodoItems.Add(testRow2);
+    dbContext.TodoItems.Add(testRow3);
+    dbContext.SaveChanges();
+    
+    transaction.Commit();
+}
+catch (Exception e)
+{
+    
+    Console.Error.WriteLine(
+        $"Error encountered while performing database operations: {e.Message}");
+    Environment.Exit(1);
+}
---
+++ TodoRwa/Models/TodoItem.cs
@@ -0,0 +1,8 @@
+namespace TodoRwa.Models;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
