6224a06889cd9a337e3a176ba883dd613a75a504
Brook Li <emcdynamicist@gmail.com>
2024-06-24 02:40:06 -0400

    Changed script to operate under the assumption that the database is externally dropped before running the script

--- SeedDatabase/Program.cs
+++ SeedDatabase/Program.cs
@@ -2,7 +2,10 @@
 using Microsoft.Extensions.Configuration;
 using TodoRwa.Models;
 
-IConfigurationRoot config = new ConfigurationBuilder()
+// The following script assumes that the database does not exist at the time that it is run. The database must be
+// dropped externally (e.g. via a shell script and psql) before running this script.
+
+var config = new ConfigurationBuilder()
     .AddUserSecrets<Program>()
     .Build();
 var postgresConnStr = config["Postgres:ConnectionString"];
@@ -10,7 +13,11 @@ var postgresConnStr = config["Postgres:ConnectionString"];
 var contextOptions = new DbContextOptionsBuilder<TodoContext>()
     .UseNpgsql(postgresConnStr).Options;
 var dbContext = new TodoContext(contextOptions);
+
 dbContext.Database.Migrate();
+// Getting the migration to work at design time seems quite problematic. However, running the migration at runtime
+// seems to be a good solution. The following answer inspired this solution: https://stackoverflow.com/a/55173783.
+
 using var transaction = dbContext.Database.BeginTransaction();
 
 try
@@ -30,8 +37,7 @@ try
         Name = null,
         IsComplete = true
     };
-
-    dbContext.Database.ExecuteSql($"DELETE FROM \"TodoItems\"");
+    
     dbContext.TodoItems.Add(testRow1);
     dbContext.TodoItems.Add(testRow2);
     dbContext.TodoItems.Add(testRow3);
