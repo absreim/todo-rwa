4244f855ecb39b799ee6f8323127aef30c2fa36b
Brook Li <emcdynamicist@gmail.com>
2024-07-02 14:17:43 -0400

    Modified seed test data script to automatically drop the test database

--- SeedDatabase/Program.cs
+++ SeedDatabase/Program.cs
@@ -1,22 +1,55 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Npgsql;
 using TodoRwa.Models;
-
-// The following script assumes that the database does not exist at the time that it is run. The database must be
-// dropped externally (e.g. via a shell script and psql) before running this script.
 
 var config = new ConfigurationBuilder()
     .AddUserSecrets<Program>()
     .Build();
+
+// Some considerations when into my decision to drop the database before each reseed instead of deleting table contents
+// or dropping tables in the database.
+// 1. Sequence numbers do not get reset by clearing out the contents of tables. Ideally, tests should not break when
+// sequence numbers change, but it is quite possible that after enough runs that sequence numbers might become large
+// enough to become an eyesore.
+// 2. Maintaining table-dropping code can be tedious. To make matters worse, some database engines understandably do not
+// allow multiple cascade paths. In schemas complex enough to require such consideration, one would likely get around
+// the issue by disabling cascade delete. Doing so would require carefully ordered dropping of tables to avoid
+// protections on dropping tables that are being referenced by foreign keys, making the table-dropping code even more
+// complicated.
+//
+// To drop a database, one must make a connection to a database other one that they want to drop. The main database
+// just happens to be conveniently exist. Theoretically, one can connect any other database to perform the drop.
+// Of course, the user account would need to have permission to connect to the database.
+var mainDbConnStr = config["Postgres:MainDbConnStr"];
+var testDbName = config["TestDbName"];
+if (testDbName == null)
+{
+    Console.Error.WriteLine("Please specify the name of the test DB in the 'TestDbName' configuration value.");
+    Environment.Exit(1);
+}
+
+var dataSourceBuilder = new NpgsqlDataSourceBuilder(mainDbConnStr);
+var dataSource = dataSourceBuilder.Build();
+await using (var conn = await dataSource.OpenConnectionAsync())
+{
+    var cmd = new NpgsqlCommand($"DROP DATABASE IF EXISTS {testDbName}", conn);
+    cmd.Parameters.AddWithValue(testDbName);
+    await cmd.ExecuteNonQueryAsync();
+}
+
 var postgresConnStr = config["Postgres:ConnectionString"];
 
 var contextOptions = new DbContextOptionsBuilder<TodoContext>()
     .UseNpgsql(postgresConnStr).Options;
 var dbContext = new TodoContext(contextOptions);
 
-dbContext.Database.Migrate();
 // Getting the migration to work at design time seems quite problematic. However, running the migration at runtime
 // seems to be a good solution. The following answer inspired this solution: https://stackoverflow.com/a/55173783.
+//
+// Runtime migrations create the database if it does not already exist. Since we are dropping the database earlier
+// in this script and are now recreating it, please ensure that the database role has permission to create a database.
+dbContext.Database.Migrate();
 
 using var transaction = dbContext.Database.BeginTransaction();
 
@@ -37,17 +70,16 @@ try
         Name = null,
         IsComplete = true
     };
-    
+
     dbContext.TodoItems.Add(testRow1);
     dbContext.TodoItems.Add(testRow2);
     dbContext.TodoItems.Add(testRow3);
     dbContext.SaveChanges();
-    
+
     transaction.Commit();
 }
 catch (Exception e)
 {
-    
     Console.Error.WriteLine(
         $"Error encountered while performing database operations: {e.Message}");
     Environment.Exit(1);
